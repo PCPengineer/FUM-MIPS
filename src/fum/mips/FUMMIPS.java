521f98ea8c8fb29fd6226ee0abb52eb60c12cbe8
Mrhb <mrhb6006@gmail.com>
2020-05-05 17:20:28 +0430

    lastInit

--- src/fum/mips/FUMMIPS.java
+++ src/fum/mips/FUMMIPS.java
@@ -15,8 +15,7 @@ public class FUMMIPS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-            //com
+        //start        
     }
     
 }
