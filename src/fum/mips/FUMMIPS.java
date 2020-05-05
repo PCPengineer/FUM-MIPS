7dcf4609393f63a96b837b87c7d7e21e24ea756d
Mrhb <mrhb6006@gmail.com>
2020-05-05 17:04:44 +0430

    test2

--- src/fum/mips/FUMMIPS.java
+++ src/fum/mips/FUMMIPS.java
@@ -16,7 +16,7 @@ public class FUMMIPS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-            //aaaaaaaaaaaaaaaaaaaa
+        
     }
     
 }
